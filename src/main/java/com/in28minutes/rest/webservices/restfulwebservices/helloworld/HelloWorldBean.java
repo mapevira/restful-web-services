410a1ecc7cdcfb17c4cf455118d85876f5652c47
Raul Perez <r.perez.vicente@ibermatica.com>
2023-07-01 16:07:52 +0200

    131. Step 03 - Enhancing the Hello World REST API to return a Bean

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldBean.java
@@ -0,0 +1,22 @@
+package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * The hello world bean.
+ *
+ * @author ayesa - architecture
+ * @since jdk 1.17
+ * @version Jun 30, 2023 - 8:30:28 PM
+ *
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class HelloWorldBean {
+
+    private String message;
+
+}
